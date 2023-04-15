70d69163b3283ca05ded2611ea5aec5b5085a164
Reales Myles <rclarence123@gmail.com>
2023-04-15 17:59:53 -0500

    Primer commit notificaciones push

---
+++ lib/screen/message_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MessageScreen extends StatelessWidget {
+  const MessageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Message'),
+      ),
+      body: Center(
+        child: Text('Message Screen', style: const TextStyle(fontSize: 30)),
+      ),
+    );
+  }
+}
